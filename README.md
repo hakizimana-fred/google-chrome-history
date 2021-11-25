db555599b3621b363c4806bc7a29f7208f390d7c
hakifred <hakifred20@gmail.com>
2021-11-25 20:26:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center">Google Chrome Browser Histories </h1>
+
+## Prerequisites
+
+- python 3.x
+- pip3
+
+
+## Main use 
+GET your Google Chrome browser histories
---
+++ history.py
@@ -0,0 +1,30 @@
+import sqlite3
+import pandas as pd
+from tabulate import tabulate
+import os
+
+
+def main():
+    chrome_path = f"{os.path.expanduser('~')}/.config/google-chrome/Default/History"
+    con = sqlite3.connect(chrome_path)
+    c = con.cursor()
+    c.execute("SELECT * FROM urls")
+
+    results = c.fetchall()
+
+    count = 0
+    histories = []
+
+    for history in results:
+        histories.append(history)
+        count += 1
+        if count == 100:
+            break
+    c.close()
+
+    pd_data = pd.DataFrame(histories)
+    return print(tabulate(pd_data, headers='keys', tablefmt='grid', showindex=False))
+
+
+if __name__ == '__main__':
+    main()
